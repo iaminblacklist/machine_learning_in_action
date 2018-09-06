2d4d86a862aeec5c8bf3efb7e3c5d51e94e233d1
OAA <iaminblacklist@gmail.com>
2018-09-06 21:28:30 +0800

    linear regression

---
+++ Linear_Regression/regression.py
@@ -0,0 +1,223 @@
+from numpy import *
+
+
+#   数据导入
+def loadDataSet(fileName):      #general function to parse tab -delimited floats
+    numFeat = len(open(fileName).readline().split('\t')) - 1 #get number of fields
+    dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr =[]
+        curLine = line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
+
+
+# 标准回归函数
+def standRegres(xArr,yArr):
+    # 读入x和y并保存到矩阵中
+    xMat = mat(xArr); yMat = mat(yArr).T
+    # 计算X^T*X
+    xTx = xMat.T*xMat
+    # 计算行列式是否为0
+    if linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    # 求出回归系数w
+    ws = xTx.I * (xMat.T*yMat)
+    return ws
+
+
+# 局部加权线性回归函数
+def lwlr(testPoint,xArr,yArr,k=1.0):
+    xMat = mat(xArr); yMat = mat(yArr).T
+    m = shape(xMat)[0]
+    # 创建对角权重矩阵weights
+    weights = mat(eye((m)))
+    # 遍历数据集，计算每个样本点对应的权重值，
+    # 随着样本点与待预测点距离的递增，权重将以指数级衰减
+    # 参数k控制衰减的速度
+    for j in range(m):                      #next 2 lines create weights matrix
+        diffMat = testPoint - xMat[j,:]     #
+        weights[j,j] = exp(diffMat*diffMat.T/(-2.0*k**2))
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+
+# 为数据集中的每个点调用lwlr()，有助于求解k的大小
+def lwlrTest(testArr,xArr,yArr,k=1.0):  #loops over all the data points and applies lwlr to each one
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i],xArr,yArr,k)
+    return yHat
+
+
+def lwlrTestPlot(xArr,yArr,k=1.0):  #same thing as lwlrTest except it sorts X first
+    yHat = zeros(shape(yArr))       #easier for plotting
+    xCopy = mat(xArr)
+    xCopy.sort(0)
+    for i in range(shape(xArr)[0]):
+        yHat[i] = lwlr(xCopy[i],xArr,yArr,k)
+    return yHat,xCopy
+
+
+def rssError(yArr,yHatArr): #yArr and yHatArr both need to be arrays
+    return ((yArr-yHatArr)**2).sum()
+
+
+# 岭回归
+def ridgeRegres(xMat, yMat, lam=0.2):
+    # 构建矩阵X^T*X
+    xTx = xMat.T * xMat
+    denom = xTx + eye(shape(xMat)[1]) * lam
+    if linalg.det(denom) == 0.0:
+        print("This matrix is singular, cannot do inverse")
+        return
+    # 如果矩阵非奇异就计算回归系数并返回
+    ws = denom.I * (xMat.T * yMat)
+    return ws
+
+
+def ridgeTest(xArr, yArr):
+    xMat = mat(xArr);
+    yMat = mat(yArr).T
+    yMean = mean(yMat, 0)
+    # 所有特征都减去各自的均值并除以方差
+    yMat = yMat - yMean  # to eliminate X0 take mean off of Y
+    # regularize X's
+    xMeans = mean(xMat, 0)  # calc mean then subtract it off
+    xVar = var(xMat, 0)  # calc variance of Xi then divide by it
+    xMat = (xMat - xMeans) / xVar
+    # 在30个不同的lambda下调用ridgeRegres()函数
+    numTestPts = 30
+    wMat = zeros((numTestPts, shape(xMat)[1]))
+    for i in range(numTestPts):
+        ws = ridgeRegres(xMat, yMat, exp(i - 10))
+        wMat[i, :] = ws.T
+    return wMat
+
+
+def regularize(xMat):#regularize by columns
+    inMat = xMat.copy()
+    inMeans = mean(inMat,0)   #calc mean then subtract it off
+    inVar = var(inMat,0)      #calc variance of Xi then divide by it
+    inMat = (inMat - inMeans)/inVar
+    return inMat
+
+
+# 前向逐步线性回归
+# xArr:输入数据 yArr:预测变量 eps:每次迭代需要调整的步长 numIt:迭代次数
+def stageWise(xArr,yArr,eps=0.01,numIt=100):
+    xMat = mat(xArr); yMat=mat(yArr).T
+    yMean = mean(yMat,0)
+    yMat = yMat - yMean     #can also regularize ys but will get smaller coef
+    xMat = regularize(xMat)
+    m,n=shape(xMat)
+    #returnMat = zeros((numIt,n)) #testing code remove
+    ws = zeros((n,1)); wsTest = ws.copy(); wsMax = ws.copy()
+    for i in range(numIt):
+        print(ws.T)
+        lowestError = inf;
+        for j in range(n):
+            for sign in [-1,1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps*sign
+                yTest = xMat*wsTest
+                rssE = rssError(yMat.A,yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        #returnMat[i,:]=ws.T
+    #return returnMat
+
+
+from time import sleep
+import json
+import urllib
+
+
+def searchForSet(retX, retY, setNum, yr, numPce, origPrc):
+    sleep(10)
+    myAPIstr = 'AIzaSyD2cR2KFyx12hXu6PFU-wrWot3NXvko8vY'
+    searchURL = 'https://www.googleapis.com/shopping/search/v1/public/products?key=%s&country=US&q=lego+%d&alt=json' % (
+    myAPIstr, setNum)
+    pg = urllib.urlopen(searchURL)
+    retDict = json.loads(pg.read())
+    for i in range(len(retDict['items'])):
+        try:
+            currItem = retDict['items'][i]
+            if currItem['product']['condition'] == 'new':
+                newFlag = 1
+            else:
+                newFlag = 0
+            listOfInv = currItem['product']['inventories']
+            for item in listOfInv:
+                sellingPrice = item['price']
+                if sellingPrice > origPrc * 0.5:
+                    print("%d\t%d\t%d\t%f\t%f" % (yr, numPce, newFlag, origPrc, sellingPrice))
+                    retX.append([yr, numPce, newFlag, origPrc])
+                    retY.append(sellingPrice)
+        except:
+            print('problem with item %d' % i)
+
+
+def setDataCollect(retX, retY):
+    searchForSet(retX, retY, 8288, 2006, 800, 49.99)
+    searchForSet(retX, retY, 10030, 2002, 3096, 269.99)
+    searchForSet(retX, retY, 10179, 2007, 5195, 499.99)
+    searchForSet(retX, retY, 10181, 2007, 3428, 199.99)
+    searchForSet(retX, retY, 10189, 2008, 5922, 299.99)
+    searchForSet(retX, retY, 10196, 2009, 3263, 249.99)
+
+
+# 交叉验证测试岭回归
+def crossValidation(xArr,yArr,numVal=10):
+    # 计算数据点的个数m
+    m = len(yArr)
+    indexList = range(m)
+    errorMat = zeros((numVal,30))#create error mat 30columns numVal rows
+    for i in range(numVal):
+        # 创建训练集和测试集容器
+        trainX=[]; trainY=[]
+        testX = []; testY = []
+        # 混洗，实现训练集或测试集数据点的随机选取
+        random.shuffle(indexList)
+        for j in range(m):#create training set based on first 90% of values in indexList
+            # 将数据集的90%分割成训练集，10%为测试集
+            if j < m*0.9:
+                trainX.append(xArr[indexList[j]])
+                trainY.append(yArr[indexList[j]])
+            else:
+                testX.append(xArr[indexList[j]])
+                testY.append(yArr[indexList[j]])
+        # wMat保存岭回归中所有回归系数
+        wMat = ridgeTest(trainX,trainY)    #get 30 weight vectors from ridge
+        for k in range(30):#loop over all of the ridge estimates
+            matTestX = mat(testX); matTrainX=mat(trainX)
+            meanTrain = mean(matTrainX,0)
+            varTrain = var(matTrainX,0)
+            matTestX = (matTestX-meanTrain)/varTrain #regularize test with training params
+            yEst = matTestX * mat(wMat[k,:]).T + mean(trainY)#test ridge results and store
+            # 计算误差
+            errorMat[i,k]=rssError(yEst.T.A,array(testY))
+            #print errorMat[i,k]
+    meanErrors = mean(errorMat,0)#calc avg performance of the different ridge weight vectors
+    minMean = float(min(meanErrors))
+    bestWeights = wMat[nonzero(meanErrors==minMean)]
+    #can unregularize to get model
+    #when we regularized we wrote Xreg = (x-meanX)/var(x)
+    #we can now write in terms of x not Xreg:  x*w/var(x) - meanX/var(x) +meanY
+    xMat = mat(xArr); yMat=mat(yArr).T
+    meanX = mean(xMat,0); varX = var(xMat,0)
+    unReg = bestWeights/varX
+    print("the best model from Ridge Regression is:\n",unReg)
+    print("with constant term: ",-1*sum(multiply(meanX,unReg)) + mean(yMat))
